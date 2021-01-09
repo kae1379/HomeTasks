f7b5c4e1e2ee22f7bf5972c87a7d3dd887474b70
KateR <ree.post@yandex.ru>
2021-01-09 18:42:40 +0300

    что-то изменилось, оно само

--- java/src/Fundamental/OptionalTask2/twoDimensionalArray.java
+++ java/src/Fundamental/OptionalTask2/twoDimensionalArray.java
@@ -4,7 +4,6 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Arrays;
-import java.util.Comparator;
 
 /**
  * В заднии достаточно выполнить 1-2 пункта
